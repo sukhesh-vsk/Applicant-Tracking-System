6b429a31febc9e9b936b3f840b258061a48bc4b2
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2023-12-13 02:26:13 +0530

    Login Component Updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import { Container, CssBaseline, Avatar, Typography, TextField, Button, createTheme } from '@mui/material';
-import { makeStyles, ThemeProvider  } from '@mui/styles';
+import { Container, CssBaseline, Avatar, Typography, TextField, Button, FormControlLabel, Checkbox, Link, Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { withTheme } from '@emotion/react';
 
-const theme = new createTheme();
+const darkTheme = createTheme({
+    palette: {
+      mode: 'light',
+    },
+});
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((darkTheme) => ({
     paper: {
         paddingTop: '100px',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     
     form: {
@@ -44,8 +48,8 @@ const LoginForm = () => {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-        <Container component='main' maxWidth='xs'>
+        <ThemeProvider theme={darkTheme}>
+        <Container component='main' maxWidth='xs' className={classes.root}>
             <CssBaseline />
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
@@ -76,15 +80,34 @@ const LoginForm = () => {
                         value = {formData.password}
                         onChange = {eventHandle}
                     />
+                    <FormControlLabel 
+                        control = {<Checkbox value = 'remember' color = 'primary'/>}
+                        label = 'Remember me'
+                    />
+                    
                     <Button 
                         type = 'submit'
                         color = 'primary'
                         variant = 'contained'
                         className = {classes.submit}
                         fullWidth
+                        sx={{ mt: 3, mb: 2 }}
                     >
                         Sign In
                     </Button>
+
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href='#' variant='body2'>
+                                Forget password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href='#' variant='body2'>
+                                Don't have account? Sign Up
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </form>
             </div>
         </Container>
