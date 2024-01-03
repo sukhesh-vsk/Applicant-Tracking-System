8c461b0318cb4ac1ca4399d83b7a5af9dfefb9e2
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-01-03 22:56:39 +0530

    Added Todo List

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
 
 import './Todo.css';
 
 const Todo = () => {
     const [todos, setTodos] = useState(["Water Plants", "Clean Room"]);
     const [inputValue, setInputValue] = useState('');
+    const [showTextArea, setShowTextArea] = useState(false);
+    const [isIconClicked, setIconClicked] = useState(false);
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
@@ -24,19 +27,53 @@ const Todo = () => {
 
     return (
         <div>
-            <span>Todo</span><br></br>
-            <input type="text" value={inputValue} onChange={handleInputChange} />
-            <button onClick={handleAddTodo}>Add Todo</button>
+            <span className='ms-3 me-4 d-flex align-items-center justify-content-between'>
+                <h4 className='m-0'>Todo</h4>
+                <FaPlus
+                    onClick={() => {
+                        setShowTextArea(!showTextArea);
+                        setIconClicked(!isIconClicked);
+                    }}
+                    className={isIconClicked ? 'rotated-icon' : 'plus'}
+                />
+            </span>
+            {
+                showTextArea && (
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <>
+                            <input
+                                type="text"
+                                value={inputValue}
+                                onChange={handleInputChange}
+                                onKeyDown={e => {
+                                    if (e.key === 'Enter') {
+                                        handleAddTodo();
+                                        e.preventDefault();
+                                    }
+                                }}
+                                style={{ fontStyle: inputValue ? 'normal' : 'italic' }}
+                                placeholder="Add Todo here..."
+                                className='ps-2 py-2 w-75 inpTextarea mt-3'
+                            />
+                        </>
+                    </div>
+                )
+            }
             <ul className='p-0'>
                 {todos.map((todo, index) => (
-                    <div className="form-check m-3" key={index}>
-                        <div>
-                        <input className="form-check-input" type="checkbox" id={`todo-${index}`} />
-                        <label className="form-check-label" htmlFor={`todo-${index}`}>
-                            {todo}
-                        </label>
+                    <li key={index} className="todo px-3 py-2 my-3 ms-4">
+                        <div className="form-check d-flex justify-content-between">
+                            <span>
+                                <input className="form-check-input" type="checkbox" id={`todo-${index}`} />
+                                <label className="form-check-label" htmlFor={`todo-${index}`}>
+                                    {todo}
+                                </label>
+                            </span>
+                            <button type="button" className="close my-auto" aria-label="Close" onClick={() => handleDeleteTodo(index)}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
-                    </div>
+                    </li>
                 ))}
             </ul>
         </div>
